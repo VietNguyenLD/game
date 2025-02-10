e8968195ea9318a9fbb12bbead7e8dce6c1a2950
VietNguyenLD <thocoqtv@gmail.com>
2025-02-10 09:39:10 +0700

    move img

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -3,38 +3,38 @@ import { Multiplier } from "../types";
 export const multipliers: Multiplier[] = [
     {
         label: "block-20",
-        img: "/src/assets/img/multipliers/multiplier20.png"
+        img: "/assets/img/multipliers/multiplier20.png"
     },
     {
         label: "block-4",
-        img: "/src/assets/img/multipliers/multiplier4.png"
+        img: "/assets/img/multipliers/multiplier4.png"
     },
     {
         label: "block-1.5",
-        img: "/src/assets/img/multipliers/multiplier1.5.png"
+        img: "/assets/img/multipliers/multiplier1.5.png"
     },
     {
         label: "block-0.3",
-        img: "/src/assets/img/multipliers/multiplier0.3.png"
+        img: "/assets/img/multipliers/multiplier0.3.png"
     },
     {
         label: "block-0.2",
-        img: "/src/assets/img/multipliers/multiplier0.2.png"
+        img: "/assets/img/multipliers/multiplier0.2.png"
     },
     {
         label: "block-0.3",
-        img: "/src/assets/img/multipliers/multiplier0.3.png"
+        img: "/assets/img/multipliers/multiplier0.3.png"
     },
     {
         label: "block-1.5",
-        img: "/src/assets/img/multipliers/multiplier1.5.png"
+        img: "/assets/img/multipliers/multiplier1.5.png"
     },
     {
         label: "block-4",
-        img: "/src/assets/img/multipliers/multiplier4.png"
+        img: "/assets/img/multipliers/multiplier4.png"
     },
     {
         label: "block-20",
-        img: "/src/assets/img/multipliers/multiplier20.png"
+        img: "/assets/img/multipliers/multiplier20.png"
     }
 ];
